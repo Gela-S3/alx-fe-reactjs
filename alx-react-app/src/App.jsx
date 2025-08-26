5f656db64ea346cee8add0948efab9665a00373a
Gelagay G. <gela.sirta3@gmail.com>
2025-08-27 02:02:35 +0300

    Header, Mainconten, and Footer Components are added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,12 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
 
   return (
     <>
-      <WelcomeMessage />
+    <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
+    
     </>
   )
 }
