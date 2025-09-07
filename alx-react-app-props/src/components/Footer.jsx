003be67cff749c9c6900ef16cd6e64be640da26a
Gelagay G. <gela.sirta3@gmail.com>
2025-09-07 22:55:42 +0300

    feat: Initialize React app with Vite, add essential components and context for user profile
    - Created package.json for project dependencies and scripts
    - Added Vite configuration for building and serving the app
    - Implemented main application structure with App component
    - Developed components: Header, Footer, MainContent, UserProfile, WelcomeMessage, UserInfo, UserDetails, and ProfilePage
    - Established UserContext for managing user data across components
    - Styled components with CSS for better presentation
    - Included SVG assets for branding

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+        <footer>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
