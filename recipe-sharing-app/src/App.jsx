f9a175c2a1e74ee640d310485d841666ad11bc5a
Gelagay G. <gela.sirta3@gmail.com>
2025-09-14 23:01:36 +0300

    feat: add updateRecipe action to modify existing recipes in the store

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+
+
 function App() {
   return (
     <Router>
